bda578e94196bfb94dd652fa5b00e571090154c3
Eric Martin <eric_martin@me.com>
2021-10-04 21:49:21 -0600

    added some functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,34 @@ var forcastEl = document.querySelector("#forcast");
 var fiveDayEl = document.querySelector("#five-day-container");
 var pastSearchEl = document.querySelector("#past-search-buttons");
 
+var submitForm = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.ariaValueMax.trim();
+    if (city) {
+        getCityWeather(city);
+        getFiveDay(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch();
+}
+
+var saveSearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+var getCityWeather = function(city) {
+    var apiKey = "00fa8b03126daafdd49a1dd442af1554";
+    var apiURL = "api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}";
+
+    fetch(apiURL)
+    .then(function(response) {
+        response.json().then(function(data) {
+            displayWeather(data, city);
+        });
+    });
+};
+
