bad7ed852436ad65c01228c58db42b6137536cb0
Eric Martin <eric_martin@me.com>
2021-10-04 21:37:01 -0600

    added global variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var cities = [];
+
+var cityFormEl = document.querySelector("#city-search");
+var cityInputEl = document.querySelector("#city");
+var citySearchEl = document.querySelector("#searched-city");
+var weatherEl = document.querySelector("#current-weather");
+var forcastEl = document.querySelector("#forcast");
+var fiveDayEl = document.querySelector("#five-day-container");
+var pastSearchEl = document.querySelector("#past-search-buttons");
+
